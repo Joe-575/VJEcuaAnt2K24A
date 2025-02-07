67bfe747b276685e1f7ac22603d0a066191e836e
Joe-575 <medinajoel814@gmail.com>
2025-02-06 21:36:04 -0500

    VJCarnivoro

--- src/VJBLC/VJEntities/VJCarnivoro.java
+++ src/VJBLC/VJEntities/VJCarnivoro.java
@@ -1,5 +1,16 @@
 package VJBLC.VJEntities;
 
-public abstract class VJCarnivoro {
+public class VJCarnivoro extends VJIngestaNativa {
+    @Override
+    public boolean inyectar(VJGenoAlimento genoAlimento) {
+        // Lógica específica para Carnívoro
+        genoAlimento.aplicarEvolucion(new VJHormiga(0));  // Puede alimentar la hormiga
+        return true;
+    }
 
+    @Override
+    protected VJXY getInyectadoCon() {
+        System.out.println("Error en VJCarnivoro");
+        throw new UnsupportedOperationException("Unimplemented method 'getInyectadoCon'");
+    }
 }
