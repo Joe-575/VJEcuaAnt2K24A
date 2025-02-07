66dafeeb7f27d445a2132d7b7b92307cb9536442
Joe-575 <medinajoel814@gmail.com>
2025-02-06 19:40:16 -0500

    VJIDAO

--- src/VJDAC/VJIDAO.java
+++ src/VJDAC/VJIDAO.java
@@ -1,5 +1,14 @@
 package VJDAC;
 
-public class VJIDAO {
-    
-}
+import java.sql.ResultSet;
+import java.util.List;
+
+public interface VJIDAO<T> {
+    T           newDTO(ResultSet rs);
+    T           readBy(Integer id) throws Exception;
+    List<T>     readAll() throws Exception;
+    boolean     create(T dto) throws Exception;
+    boolean     update(T dto) throws Exception;
+    boolean     delete(Integer id) throws Exception;
+    Integer     getMaxRow() throws Exception;
+} 
